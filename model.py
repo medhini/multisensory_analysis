6ac11add03b67cbc0beb0606976ea3873dc5adc6
Medhini Narasimhan <medhini@wirelessprv-10-194-122-106.near.illinois.edu>
2018-11-13 16:53:37 -0600

    Add model

---
+++ model.py
@@ -0,0 +1,13 @@
+import sys
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+class alignment(nn.Module):
+    def __init__(self, bathcsize):
+
+    def forward(self, batchsize):
