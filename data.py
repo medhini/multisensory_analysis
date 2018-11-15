6fded5a1f0e8c3ae6e27e53044a5971f3a750675
Medhini Narasimhan <medhini@wirelessprv-10-194-122-106.near.illinois.edu>
2018-11-15 16:02:10 -0600

    Fixed type data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@
 import matplotlib.pyplot as plt
 import torch 
 import torch.nn as nn
-import torchvisions
+import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 import h5py  
