9d88ddfaa7819c9ca08996015a13bd1be63cc598
Medhini <medhini95@gmail.com>
2018-11-29 10:15:03 -0600

    Fix image

--- model.py
+++ model.py
@@ -41,12 +41,12 @@ class Block2(nn.Module):
 class Block3(nn.Module):
     expansion = 1
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride, downsample=None):
+    def __init__(self, in_channels, out_channels, kernel_size=(1,1,1), stride=1, downsample=None):
         super(Block3, self).__init__()
         self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size, stride, padding=0, dilation=1, groups=1, bias=True)
         self.bn1 = nn.BatchNorm3d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=(1,1,1), stride=(1,1,1))
+        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=(1,1,1), stride=1)
         self.bn2 = nn.BatchNorm3d(out_channels)
         self.downsample = downsample
         self.stride = stride
@@ -91,7 +91,7 @@ class alignment(nn.Module):
         self.im_net_1 = self._make_layer(Block3, 64, 64, (3,3,3), (2,2,2), 2)
 
         """Fuse Features"""
-        self.conv3_2 = nn.Conv3d(64, 512, (1, 1, 1))
+        self.conv3_2 = nn.Conv3d(1, 512, (1, 1, 1))
         self.conv3_3 = nn.Conv3d(512, 128, (1, 1, 1))
         self.joint_net_1 = self._make_layer(Block3, 128, 128, (3,3,3), (2,2,2), 2)
         self.joint_net_2 = self._make_layer(Block3, 128, 256, (3,3,3), (1,2,2), 2)
@@ -116,12 +116,10 @@ class alignment(nn.Module):
         layers = []
         layers.append(block(in_channels, out_channels, kernel_size, stride, downsample))
         for _ in range(1, blocks):
-            if stride != 1:
-                layers.append(block(out_channels, out_channels, kernel_size, stride, downsample))
-            else:
-                layers.append(block(out_channels, out_channels, kernel_size, stride))
+            layers.append(block(out_channels, out_channels))
 
         return nn.Sequential(*layers)
+
     
 
     # def block_2(self, in_channels, out_channels, kernel_size, stride, downsample=None):
